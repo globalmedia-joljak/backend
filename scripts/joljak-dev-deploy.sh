e93fa02472e502b840b5ee3d51d8ad6bf4312bcd
chlalstjd430 <cms3136@gmail.com>
2021-02-27 02:16:46 +0900

    dev환경 ci/cd 적용

--- scripts/joljak-dev-deploy.sh
+++ scripts/joljak-dev-deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 REPOSITORY=/home/ubuntu/dev/backend/build
 PROJECT_NAME=joljak-dev-server
@@ -7,14 +7,25 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/*.jar $REPOSITORY/zip
 
-echo "> 구동중인 애플리케이션 pid가 있다면 죽입니다"
-fuser -k -n tcp 8080
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-echo "> 새 애플리케이션 배포"
+CURRENT_PID=$(pgrep -f *.jar | awk '{print  $1}')
+echo "현재 구동중인 애플리케이션 pid:" $CURRENT_PID
+
+if [  -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> 현재 구동 중인 애플리케이션 삭제."
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> 새애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/zip/*.jar | tail -n 1)
 
-echo "> JAR Name: $JAR_NAME"
+echo "> JAR Name:$JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
@@ -22,6 +33,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-source $HOME/.bashrc
+sudo nohup java -jar -Dspring.config.location=/home/ubuntu/dev/backend/config/application-dev.properties $JAR_NAME > $REPOSITORY/zip/nohup.out 2>&1 &
 
-nohup java -jar -Dspring.config.location=/home/ubuntu/dev/backend/config/application-dev.properties $JAR_NAME > $REPOSITORY/zip/nohup.out 2>&1 &
+echo "> $JAR_NAME 실행 완료"
