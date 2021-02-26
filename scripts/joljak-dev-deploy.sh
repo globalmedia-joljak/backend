fc31975b6132883248eb9bd59247126990012404
chlalstjd430 <cms3136@gmail.com>
2021-02-27 02:41:43 +0900

    dev환경 ci/cd 적용

--- scripts/joljak-dev-deploy.sh
+++ scripts/joljak-dev-deploy.sh
@@ -7,19 +7,7 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/*.jar $REPOSITORY/zip
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -f *.jar | awk '{print  $1}')
-echo "현재 구동중인 애플리케이션 pid:" $CURRENT_PID
-
-if [  -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-        echo "> 현재 구동 중인 애플리케이션 삭제."
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
-fi
+sudo kill $(sudo lsof -t -i:8080)
 
 echo "> 새애플리케이션 배포"
 
