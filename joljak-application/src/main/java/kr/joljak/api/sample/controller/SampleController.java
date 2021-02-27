97ab64ff295f16d3f484fb5a20355fd3e261cf3b
chlalstjd430 <cms3136@gmail.com>
2021-02-27 16:44:20 +0900

    - swagger config 적용 - test controller에 @ApiOperation 적용 - JPA Config 적용

--- joljak-application/src/main/java/kr/joljak/api/sample/controller/SampleController.java
+++ joljak-application/src/main/java/kr/joljak/api/sample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package kr.joljak.api.sample.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/tests")
 public class SampleController {
 
+  @ApiOperation("테스트 컨트롤러")
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public String test() {
