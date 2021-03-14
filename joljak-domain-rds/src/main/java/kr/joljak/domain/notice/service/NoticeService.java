d19324499df45b3f3ce3a7cd25a9c908c517d6c4
hamryt <rkdl0829@naver.com>
2021-03-14 10:53:31 +0900

    Service Test 추가 및 코드 스타일 수정

--- joljak-domain-rds/src/main/java/kr/joljak/domain/notice/service/NoticeService.java
+++ joljak-domain-rds/src/main/java/kr/joljak/domain/notice/service/NoticeService.java
@@ -34,12 +34,6 @@ public class NoticeService {
     return noticeRepository.findAll(pageable).getContent();
   }
 
-  @Transactional(readOnly = true)
-  public Notice getNoticeById(Long id) {
-    return noticeRepository.findById(id)
-      .orElseThrow(() -> new NoticeNotFoundException());
-  }
-
   @Transactional
   public Notice updateNotice(Long id, SimpleNoticeRequest simpleNoticeRequest) {
     Notice notice = getNoticeById(id);
@@ -47,5 +41,11 @@ public class NoticeService {
     notice.setTitle(simpleNoticeRequest.getTitle());
     notice.setContent(simpleNoticeRequest.getContent());
     return notice;
+  }
+
+  @Transactional(readOnly = true)
+  public Notice getNoticeById(Long id) {
+    return noticeRepository.findById(id)
+      .orElseThrow(() -> new NoticeNotFoundException());
   }
 }
