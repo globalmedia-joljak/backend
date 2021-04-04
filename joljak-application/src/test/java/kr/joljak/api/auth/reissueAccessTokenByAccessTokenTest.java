e7b7f949954fe44cb05924bcdc5663e80f7269b7
chlalstjd430 <cms3136@gmail.com>
2021-04-04 18:55:59 +0900

    테스트 코드 작성

--- joljak-application/src/test/java/kr/joljak/api/auth/reissueAccessTokenByAccessTokenTest.java
+++ joljak-application/src/test/java/kr/joljak/api/auth/reissueAccessTokenByAccessTokenTest.java
@@ -8,33 +8,34 @@ import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
-public class ReissueAccessTokenTest extends CommonApiTest {
+public class reissueAccessTokenByAccessTokenTest extends CommonApiTest {
 
   @Test
   public void reissueAccessToken_Success() throws Exception {
     // given
-    String refreshToken = getUserRefreshToken();
+    String accessToken = getUserAccessToken();
 
     //when
     MvcResult mvcResult = mockMvc.perform(
       post(AUTH_URL + "/reissue/accesstoken")
-        .header("refreshToken", refreshToken)
+        .header("accessToken", accessToken)
         .contentType(MediaType.APPLICATION_JSON_VALUE)
     ).andReturn();
 
     //then
+    System.out.println(mvcResult.getResponse().getErrorMessage());
     assertEquals(200, mvcResult.getResponse().getStatus());
   }
 
   @Test
-  public void reissueAccessToken_Fail_IsNotRefreshToken() throws Exception {
+  public void reissueAccessToken_Fail_IsNotAccessToken() throws Exception {
     // given
-    String refreshToken = getUserAccessToken();
+    String refreshToken = getAdminRefreshToken();
 
     //when
     MvcResult mvcResult = mockMvc.perform(
         post(AUTH_URL + "/reissue/accesstoken")
-            .header("refreshToken", refreshToken)
+            .header("accessToken", refreshToken)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
     ).andReturn();
 
@@ -43,13 +44,13 @@ public class ReissueAccessTokenTest extends CommonApiTest {
   }
 
   @Test
-  public void reissueAccessToken_Fail_InvalidToekn() throws Exception {
+  public void reissueAccessToken_Fail_InvalidToken() throws Exception {
     // given
     String refreshToken = getUserAccessToken() + nextId++;
 
     //when
     MvcResult mvcResult = mockMvc.perform(
-        post(AUTH_URL + "/reissue/accesstoken")
+        post(AUTH_URL + "/refreshtoken/reissue/accesstoken")
             .header("refreshToken", refreshToken)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
     ).andReturn();
