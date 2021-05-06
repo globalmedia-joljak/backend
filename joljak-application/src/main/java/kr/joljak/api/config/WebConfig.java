df71d2539635b29a9b435051b8a2d8a349469d58
chlalstjd430 <cms3136@gmail.com>
2021-05-06 20:48:27 +0900

    cors 해결

--- joljak-application/src/main/java/kr/joljak/api/config/WebConfig.java
+++ joljak-application/src/main/java/kr/joljak/api/config/WebConfig.java
@@ -1,6 +1,7 @@
 package kr.joljak.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +11,16 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
       .allowCredentials(false)
-      .allowedOrigins("*")
+      .allowedOrigins(
+          HttpMethod.OPTIONS.name(),
+          HttpMethod.GET.name(),
+          HttpMethod.POST.name(),
+          HttpMethod.PUT.name(),
+          HttpMethod.PATCH.name(),
+          HttpMethod.DELETE.name(),
+          HttpMethod.HEAD.name(),
+          HttpMethod.TRACE.name()
+      )
       .allowedMethods("*")
       .allowedHeaders("*");
   }
